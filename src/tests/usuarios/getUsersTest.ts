0e26be941be40112b380598da9f92c643b786978
Rhaynner Costa <rhaynner@me.com>
2025-11-18 20:34:48 -0300

    ajustes

--- src/tests/usuarios/getUsersTest.ts
+++ src/tests/usuarios/getUsersTest.ts
@@ -1,40 +1,40 @@
 import { buscaUsuarios } from '../../support/request/usuarios/getUsersService';
 import { expect } from 'chai';
 
-describe('API Serverest - Usuários', () => {
+describe('API Serverest - usuarios', () => {
   before(async () => {
     console.log('Preparando ambiente...testando o before');
   });
 
 
-  describe('Testa a api', () => {
-    it('deve retornar todos os usuários', async () => {
+  describe('Testa os parametros da api', () => {
+    it('deve retornar todos os usuarios', async () => {
       const { response, respBody } = await buscaUsuarios();
       expect(response.status).to.equal(200);
       expect(respBody.usuarios).to.be.an('array');
 
     });
 
-    it('deve retornar usuário pelo nome', async () => {
+    it('deve retornar usuario pelo nome', async () => {
       const { response, respBody } = await buscaUsuarios({ nome: 'Fulano da Silva' });
       expect(response.status).to.equal(200);
       expect(respBody.usuarios).to.be.an('array').and.not.empty;
       const usuarioEncontrado = respBody.usuarios.some((usuario: any) => usuario.nome === 'Fulano da Silva');
-      expect(usuarioEncontrado, 'Nenhum usuário com o nome "Fulano da Silva" foi encontrado').to.be.true;
+      expect(usuarioEncontrado, 'Nenhum usuario com o nome "Fulano da Silva" foi encontrado').to.be.true;
 
     });
 
-    it('deve retornar usuário pelo email', async () => {
+    it('deve retornar usuario pelo email', async () => {
       const { response, respBody } = await buscaUsuarios({ email: 'fulano@qa.com' });
       expect(response.status).to.equal(200)
       expect(respBody.usuarios).to.be.an('array')
       expect(respBody.usuarios).not.empty;
       const usuarioEncontrado = respBody.usuarios.some((usuario: any) => usuario.email === 'fulano@qa.com');
-      expect(usuarioEncontrado, 'Nenhum usuário com o email "fulano@qa.com" foi encontrado').to.be.true;
+      expect(usuarioEncontrado, 'Nenhum usuario com o email "fulano@qa.com" foi encontrado').to.be.true;
 
     })
 
-    it('nao deve retornar usuário com email nao cadastrado', async () => {
+    it('nao deve retornar usuario com email nao cadastrado', async () => {
       const { response, respBody } = await buscaUsuarios({ email: '46535243363636@qa.com' });
       expect(response.status).to.equal(200)
       expect(respBody.usuarios).to.be.an('array')
@@ -42,33 +42,33 @@ describe('API Serverest - Usuários', () => {
 
     })
 
-    it('deve retornar usuário no fitro pelo _id', async () => {
+    it('deve retornar usuario no filtro pelo _id', async () => {
       const { response, respBody } = await buscaUsuarios({ _id: '0uxuPY0cbmQhpEz1' });
       expect(response.status).to.equal(200)
       expect(respBody.usuarios).to.be.an('array')
       expect(respBody.usuarios).not.empty;
       const usuarioEncontrado = respBody.usuarios.some((usuario: any) => usuario._id === '0uxuPY0cbmQhpEz1');
-      expect(usuarioEncontrado, 'Nenhum usuário com o id "0uxuPY0cbmQhpEz1 foi encontrado').to.be.true;
+      expect(usuarioEncontrado, 'Nenhum usuario com o id "0uxuPY0cbmQhpEz1 foi encontrado').to.be.true;
 
     })
 
-    it('deve retornar usuário no fitro pelo administrador iqual a true ', async () => {
+    it('deve retornar usuario no filtro pelo administrador iqual a true ', async () => {
       const { response, respBody } = await buscaUsuarios({ administrador: 'true' });
       expect(response.status).to.equal(200)
       expect(respBody.usuarios).to.be.an('array')
       expect(respBody.usuarios).not.empty;
       const usuarioEncontrado = respBody.usuarios.some((usuario: any) => usuario.administrador === 'true');
-      expect(usuarioEncontrado, 'Nenhum usuário administrador iqual a true foi encontrado').to.be.true;
+      expect(usuarioEncontrado, 'Nenhum usuario administrador iqual a true foi encontrado').to.be.true;
 
     })
 
-    it('deve retornar usuário no fitro pelo administrador iqual a false', async () => {
+    it('deve retornar usuario no fitro pelo administrador iqual a false', async () => {
       const { response, respBody } = await buscaUsuarios({ administrador: 'false' });
       expect(response.status).to.equal(200)
       expect(respBody.usuarios).to.be.an('array')
       expect(respBody.usuarios).not.empty;
       const usuarioEncontrado = respBody.usuarios.some((usuario: any) => usuario.administrador === 'false');
-      expect(usuarioEncontrado, 'Nenhum usuário administrador iqual a false foi encontrado').to.be.true;
+      expect(usuarioEncontrado, 'Nenhum usuario administrador iqual a false foi encontrado').to.be.true;
       
     })
 
